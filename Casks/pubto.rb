a493d5392f03974a12da60e2b75550fd3ecd7131
vertex-ai-llc <pasiwang@qq.com>
2026-08-30 20:05:06 +0800

    Update Pubto v0.4.4 Cask

--- Casks/pubto.rb
+++ Casks/pubto.rb
@@ -1,14 +1,14 @@
 cask "pubto" do
-  version "0.4.3"
+  version "0.4.4"
 
   on_intel do
-    url "https://github.com/vertex-ai-llc/pubto-downloads/releases/download/v0.4.3/pubto-desktop-macos-x64.tar.gz"
-    sha256 "cde75c6056daa06fa154af8479cbb2359a91bcc7fae2610c3b53cedc3e32d1fc"
+    url "https://github.com/vertex-ai-llc/pubto-downloads/releases/download/v0.4.4/pubto-desktop-macos-x64.tar.gz"
+    sha256 "c91fe51ec2409bf98a9b7f6236a95dfecb814808cf55b4ede42d6d7238e31e01"
   end
 
   on_arm do
-    url "https://github.com/vertex-ai-llc/pubto-downloads/releases/download/v0.4.3/pubto-desktop-macos-arm64.tar.gz"
-    sha256 "1b5d9c2ea443967db065c8468eaa49d5eb2e674f4007cfc4857769651ba99fe3"
+    url "https://github.com/vertex-ai-llc/pubto-downloads/releases/download/v0.4.4/pubto-desktop-macos-arm64.tar.gz"
+    sha256 "ed561cea051df444b4ee50589997a80c613b7fcabdc7e62a520a2cd28f88ec1c"
   end
 
   name "Pubto"
